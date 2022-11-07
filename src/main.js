a126691c420fda1463d37be64882f928bcea1976
Januha88 <januharyg@gmail.com>
2022-11-06 21:32:31 -0300

    Cambios en el Dom data.js y CSS

--- src/main.js
+++ src/main.js
@@ -1,25 +1,4 @@
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+data.pokemon.forEach((pokemon) => {
 
-//console.log(data.pokemon[156].name);
-//console.log(([]).forEach);
-
-data.pokemon.forEach( (pokemon, i) =>{
-   // console.log(pokemon.img);
-    //aquí se imprime la imágen de cada pokemon contenido en el arreglo.
-   img(pokemon.img);
+    contenedorPpal(pokemon)
 })
-  function img (linkImg){
-    let imagen = document.createElement("img");
-    let elemento = document.getElementById("root");
-    imagen.src = linkImg;
-    elemento.appendChild(imagen);
-    //document.body.appendChild(imagen);
-    console.log(imagen);
-}
-
-
-
-
