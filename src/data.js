ae659955c0accc4886d674d6fcda779530c56edf
PameSegovia <114256835+PameSegovia@users.noreply.github.com>
2022-11-09 22:27:19 -0300

    Janu, envio cambios de HTML y data.js

--- src/data.js
+++ src/data.js
@@ -1,88 +1,10 @@
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import {example} from './data.js';
+//import data from './data/pokemon/pokemon.js';
 
-//console.log(data.pokemon[156].name);
-//console.log(([]).forEach);
-
-
-function img(linkImg) {
-    let contenedor = document.createElement("div")
-    let imagen = document.createElement("img");
-    imagen.src = linkImg;
-    contenedor.appendChild(imagen);
-    //document.body.appendChild(imagen);
-    contenedor.classList.add("contenedor-img");
-    imagen.classList.add("imagen-pokemon");
-
-    return contenedor;
+export function resumeData (arregloPokemon){
+  const primerosDatos = [];
+  arregloPokemon.forEach(element => {
+    primerosDatos.push({imagen:element.img, numero:element.num, nombre:element.name})
+  });
+return primerosDatos
 }
-
-function names(nombres) {
-    let contenedor = document.createElement("article");
-    let parrafo = document.createElement("p");
-    parrafo.innerHTML = nombres;
-    contenedor.appendChild(parrafo);
-    //le estoy agregando una clase al elemento contenedor
-    contenedor.classList.add("contenedor-nombre");
-
-    return contenedor;
-}
-
-
-function descripcion(informacion) {
-    let contenedor = document.createElement("article");
-    let parrafo = document.createElement("p");
-    parrafo.innerHTML = informacion;
-    contenedor.appendChild(parrafo);
-    //le estoy agragndo una clase al elemento contenedor
-    contenedor.classList.add("contenedor-descripcion");
-
-    return contenedor;
-}
-
-
-
-function contenedorPpal(pokemon) {
-    let contenedor = document.createElement("div")
-    let root = document.getElementById("root")
-    root.appendChild(contenedor);
-    contenedor.appendChild(img(pokemon.img));
-    contenedor.appendChild(names(pokemon.name));
-    contenedor.appendChild(descripcion(pokemon.about));
-    contenedor.appendChild(debilidad(pokemon.weaknesses));
-    contenedor.appendChild(resistencia(pokemon.resistant));
-    //le estoy agragndo una clase al elemento contenedor
-    contenedor.classList.add("contenedor-ppal");
-
-}
-
-function debilidad(debilidades) {
-    let contenedor = document.createElement("article");
-    debilidades.forEach((debilidad) => {
-        let span = document.createElement("span");
-        contenedor.appendChild(span);
-        span.innerHTML = debilidad;
-        span.classList.add("item-debilidad");
-    })
-    //le estoy agragndo una clase al elemento contenedor
-    contenedor.classList.add("contenedor-debilidades");
-
-    return contenedor;
-}
-
-function resistencia(resistencias) {
-    let contenedor = document.createElement("article");
-    resistencias.forEach((resistencia) => {
-        let span = document.createElement("span");
-        contenedor.appendChild(span);
-        span.innerHTML = resistencia;
-        span.classList.add("item-resistencia");
-    })
-    //le estoy agragndo una clase al elemento contenedor
-    contenedor.classList.add("contenedor-resistencias");
-
-    return contenedor;
-}
-
--- src/main.js
+++ src/main.js
@@ -1,31 +1,30 @@
 import data from './data/pokemon/pokemon.js';
+import { resumeData } from './data.js';
 
+const arregloPokemon = data.pokemon
 const btnConocerMas = document.getElementById('conocerMas');
 const pantallaUno = document.getElementById('contenedor1');
 const pantallaDos = document.getElementById('root');
 
 //FUNCIÓN PARA BOTÓN "CONOCE MÁS" Y CAMBIO DE PANTALLA//
-btnConocerMas.addEventListener("click", () => {
+btnConocerMas.addEventListener("click", (event) => {
+   event.preventDefault()
    pantallaUno.style.display = "none";
-   pantallaDos.style.display = "block"
-   })
- 
-//aquí se imprime la imágen de cada pokemon contenido en el arreglo.
-data.pokemon.forEach( (pokemon, i) =>{
-    img(pokemon.img);
-})
-  function img (linkImg){
-   let imagen = document.createElement("img");
-   let elemento = document.getElementById("imgPokemon");
-    imagen.src = linkImg;
-    elemento.appendChild(imagen);
-    document.body.appendChild(imagen);
-}
+   pantallaDos.style.display = "flex";
+});
+const pokeCard = document.getElementById("root")
+const mostrarData = resumeData(arregloPokemon);
+pokeCard.innerHTML = "";
+mostrarData.forEach(element => {
 
-//FUNCION CREAR TARJETAS POKEMÓN
+   pokeCard.insertAdjacentHTML("beforeend", `<div class="pokemonCard" id="pokemonCard">
+   <div class="img-container">
+    <img id="imgPokemon" src="${element.imagen}" alt="">
+   </div>
+   <div class="info">
+   <span class="number" id="number">"${element.numero}"</span>
+   <h3 claass="name" id="name">"${element.nombre}" </h3>
+   </div>
+   </div>`);
+});
 
- 
-//data.pokemon.forEach((pokemon) => {
-
-  //  contenedorPpal(pokemon)
-//})
