555e5c27da7ea15a112dfd991623440d19557df6
Januha88 <januharyg@gmail.com>
2022-11-09 23:36:44 -0300

    cambios en el main, data y HTML

--- src/data.js
+++ src/data.js
@@ -1,10 +1,31 @@
 //import {example} from './data.js';
 //import data from './data/pokemon/pokemon.js';
+//import data from "./data/pokemon/pokemon.js";
 
-export function resumeData (arregloPokemon){
+//estoy exportando la función
+export function resumeData(arregloPokemon) {
   const primerosDatos = [];
   arregloPokemon.forEach(element => {
-    primerosDatos.push({imagen:element.img, numero:element.num, nombre:element.name})
+    primerosDatos.push({ imagen: element.img, numero: element.num, nombre: element.name })
   });
-return primerosDatos
+  return primerosDatos
 }
+
+//estoy exportando la función
+export function filtrarPokemonPorTipo(pokemones, tipoDePokeSeleccionadoPorUsuario) {
+  return pokemones.filter((pokemon) => {
+    return pokemon.type.includes(tipoDePokeSeleccionadoPorUsuario)
+  })
+}
+
+
+export function buscarPokemonPorNombre(pokemones, nombreDePokemon) {
+  return pokemones.filter((pokemon) => {
+    return pokemon.name.includes(nombreDePokemon)
+
+  })
+}
+//variedades.forEach((variedad)=> {
+//let  pruebaResult= filtrarPokemon(data.pokemon, variedad);
+//console.log(variedad, pruebaResult)
+//})
